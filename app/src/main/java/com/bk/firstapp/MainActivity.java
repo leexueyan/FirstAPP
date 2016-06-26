6b8c29c34110b50929e43e9a23eaadcc2697087c
BK <yaobkun@outlook.com>
2016-06-26 10:47:08 +0800

    the first version does not work while this one does

--- app/src/main/java/com/bk/firstapp/MainActivity.java
+++ app/src/main/java/com/bk/firstapp/MainActivity.java
@@ -7,13 +7,29 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+    Button btn0;
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = new Button(this);
-        button.setOnClickListener(this);
+        btn0 = (Button)findViewById(R.id.build_btn);
+        btn0.setOnClickListener(this);
+        btn1= (Button) findViewById(R.id.cap_btn);
+        btn1.setOnClickListener(this);
+        btn2 = (Button) findViewById(R.id.go_btn);
+        btn2.setOnClickListener(this);
+        btn3 = (Button) findViewById(R.id.make_btn);
+        btn3.setOnClickListener(this);
+        btn4 = (Button) findViewById(R.id.movie_btn);
+        btn4.setOnClickListener(this);
+        btn5 = (Button) findViewById(R.id.stock_btn);
+        btn5.setOnClickListener(this);
     }
 
     @Override
@@ -21,16 +37,22 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         switch (v.getId()){
             case R.id.movie_btn:
                 GenerateToast("This button will launch my movie app!");
+                break;
             case R.id.stock_btn:
                 GenerateToast("This button will launch my stock app!");
+                break;
             case R.id.build_btn:
                 GenerateToast("This button will launch my build bigger app!");
+                break;
             case R.id.make_btn:
                 GenerateToast("This button will launch my material app!");
+                break;
             case R.id.go_btn:
                 GenerateToast("This button will launch my go ubi app!");
+                break;
             case R.id.cap_btn:
                 GenerateToast("This button will launch my capstone app!");
+                break;
             default:
                     break;
         }
